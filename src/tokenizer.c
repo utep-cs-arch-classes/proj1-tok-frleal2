14ff3fc345ba8417709745b9622b26b0daadf8d0
frleal2 <45012732+frleal2@users.noreply.github.com>
2021-03-10 11:45:01 -0700

    Update tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,79 +2,65 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/*Function to count the number of characters in a string*/
-
-int length_str(char* str){
-  int count = 0;
-  while(*str++){
-    ++count;
+char *copy_str(char *src, short len){
+  char *ptr = (char *)malloc(sizeof(char) * (len + 1));
+  for(int i = 0; i < len && src[i] != '\0'; i++){
+    ptr[i] = src[i];
   }
-  
-  return count;
+  ptr[len] = '\0';
+  return ptr;
 }
 
-/*Evaluates weather a character is valid or invalid*/
-char is_valid_character(char c){
+/*Evaluates weather a character is valid*/
+int space_char(char c){
   switch(c){
     case '\n':
     case '\t':
-    case '\b':
-    case '\a':
-    case '\f':
-    case '\r':
-    case '\v':
-    case '\\':
-    case '\'':
-    case '\"':
     case ' ':
-    case '\0':
-      return 0;
-    default:
       return 1;
+    default:
+      return 0;
   }
+}
+
+/*Evaluates weather a character is invalid*/
+int non_space_char(char c){
+  if(c != '\t && c != ' ' && c != '\n'){
+     return 1;
+  }
+  else{
+    return 0;
+  }  
 }
 
 /*Function to find the start of a word*/
-int find_word_start(char* sentence, int index){
-  if(!is_valid_character(sentence[index])){ //first we check if we are in a valid character, if not we look for one
-    while(!is_valid_character(sentence[index])){
-      index++;
-    }
-  }else{//we are in a valid character, so we are the word beginning
-    return index;
-  }
-  return index;
 
+char *word_start(char *str){
+  while(space_char(*str) == 1){
+    str++
+  }  
+  return str;
 }
 
 /*Function to find the end of a word*/
-int find_word_end(char* sentence, int index){
-  int beg_of_word = find_word_start(sentence, index);
-  //Now having the beginning we can get to the end
-  while(is_valid_character(sentence[beg_of_word])){
-    beg_of_word++;//when the loop is done this will be the end of the word
+char *word_end(char *str){
+  while(non_space_char(*str) == 1 && *str != '\0'){
+    str++;
   }
-  //here we went one over the end so we just return
-  beg_of_word--;
-  return beg_of_word;
-
+  return str;
 }
 
 /*Function to find the number of words there is in a sentence or sentence*/
-int count_words(char* sentence){
-    int count = 0;//counts words
-    int index = 0;
-    short end = 0;
-    short length = length_str(sentence);
-    while(1){
-      index = find_word_start(sentence, index);
-      if(index>=length) break;
-      count ++;
-      end = find_word_end(sentence, index);
-      index = end+1;
-    }
-    return count;
+int count_words(char *str){
+  int counter = 0;
+  while(*str != '\0'){
+    str = word_start(str);
+    str = word_end(str);
+    counter++;
   }
+  return counter;
+  
+}
 
 /* Function that prints all tokens*/
 void print_tokens(char** tokens){
@@ -102,13 +88,25 @@ char** tokenize(char* sentence){
   char** tokens = (char**) malloc((words_num+1)*sizeof(char*));//we allocate memory for the number of pointers needed
   
   for(short i=0;i<words_num;i++){ //we will calculate length of each string and allocate memory for each
-    word_leng = (find_word_end(sentence, index) - find_word_start(sentence, index)) + 1;
+    
+    char *start = word_start(sentence);
+    
+    char *end = word_end(sentence);
+    
+    int word_leng = (int)(end - start) + 1;
+    
     tokens[i] =(char *) malloc((word_leng + 1)*sizeof(char));//we allocate one extra for the NULL character
-    index = find_word_start(sentence, index);
+    
+    index = (int) word_start(sentence);
+    
     for(short j = 0;j<word_leng;j++){
+      
       tokens[i][j] = sentence[index+j];
+      
     }
-    index = (find_word_end(sentence, index) +1);
+    
+    index = ((int) word_end(sentence) +1);
+  
   }
   tokens[words_num] = (char*) malloc(sizeof(char));
   tokens[words_num] = '\0';
